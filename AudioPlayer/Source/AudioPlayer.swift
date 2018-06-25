1abb99c3caa6bfae44395f40ae33cb24d25eb1c2
Zero <zero.griffin@gmail.com>
2018-06-25 13:52:02 +0200

    Import audio player

--- AudioPlayer/Source/AudioPlayer.swift
+++ AudioPlayer/Source/AudioPlayer.swift
@@ -1,9 +1,7 @@
-//
-//  AudioPlayer.swift
-//  AudioPlayer
-//
-//  Created by zero on 25.06.18.
-//  Copyright © 2018 Blinkist. All rights reserved.
-//
-
 import Foundation
+
+public class Player {
+    public init() {
+        
+    }
+}
--- Demo/Source/Interactor.swift
+++ Demo/Source/Interactor.swift
@@ -1,5 +1,10 @@
 import Foundation
+import AudioPlayer
 
 class Interactor {
+    var player:Player
     
+    init() {
+        self.player = Player()
+    }
 }
