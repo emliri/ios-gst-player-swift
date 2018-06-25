14a55c89d802b341456238fe0c6f79443b740c58
Zero <zero.griffin@gmail.com>
2018-06-25 12:33:55 +0200

    Create project

---
+++ AudioPlayer/AudioPlayer/AudioPlayer.h
@@ -0,0 +1,19 @@
+//
+//  AudioPlayer.h
+//  AudioPlayer
+//
+//  Created by zero on 25.06.18.
+//  Copyright © 2018 Blinkist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioPlayer.
+FOUNDATION_EXPORT double AudioPlayerVersionNumber;
+
+//! Project version string for AudioPlayer.
+FOUNDATION_EXPORT const unsigned char AudioPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioPlayer/PublicHeader.h>
+
+
