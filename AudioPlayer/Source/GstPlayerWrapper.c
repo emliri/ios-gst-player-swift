e51a37dc543018d51bb4b95f17435ea9e4f12c8a
Stephan Hesse <tchakabam@gmail.com>
2018-06-26 15:20:37 +0200

    use g_once for call to gst_init

--- AudioPlayer/Source/GstPlayerWrapper.c
+++ AudioPlayer/Source/GstPlayerWrapper.c
@@ -10,7 +10,13 @@
 
 #include "GstPlayerWrapper.h"
 
-void* CreateGstPlayer() {
+void InitGStreamer() {
     gst_init(NULL, NULL);
+}
+
+void* CreateGstPlayer() {
+    static GOnce gst_init_once = G_ONCE_INIT;
+    g_once (&gst_init_once, InitGStreamer, NULL);
+
     return gst_player_new(NULL, NULL);
 }
