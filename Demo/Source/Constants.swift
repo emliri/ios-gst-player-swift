d4f4d20909f4ef7b823a376a4fbfd4f5f8c1de49
Zero <zero.griffin@gmail.com>
2018-07-27 13:47:54 +0200

    Create playlist struct

--- Demo/Source/Constants.swift
+++ Demo/Source/Constants.swift
@@ -3,4 +3,7 @@ import Foundation
 struct Constants {
     static let local:String = "guitars.m4a"
     static let remote:String = "https://github.com/cdmx/MiniMancera/raw/master/miniMancera/Support/Assets/sounds/soundWhistlesVsZombies.caf"
+    static let list:[String] = ["Black_Acid_Prophecy_-_14_-_Servant_of_the_Storm.mp3",
+                                "Medusah_Black_-_07_-_Guilty_Pleasure.mp3",
+                                "Space_Heater_-_04_-_Mode_7.mp3"]
 }
