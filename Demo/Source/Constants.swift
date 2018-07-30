244129f39ca09409385b5e4198b357fd01945377
Zero <zero.griffin@gmail.com>
2018-07-30 09:40:39 +0200

    Put back remote url fo no ssl

--- Demo/Source/Constants.swift
+++ Demo/Source/Constants.swift
@@ -2,7 +2,8 @@ import Foundation
 
 struct Constants {
     static let local:String = "guitars.m4a"
-    static let remote:String = "https://github.com/cdmx/MiniMancera/raw/master/miniMancera/Support/Assets/sounds/soundWhistlesVsZombies.caf"
+    static let remote:String = "http://tchakabam.com/test-media/m4a/shalafon.mp3"
+    static let remoteSsl:String = "https://github.com/cdmx/MiniMancera/raw/master/miniMancera/Support/Assets/sounds/soundWhistlesVsZombies.caf"
     static let list:[String] = ["Black_Acid_Prophecy_-_14_-_Servant_of_the_Storm.mp3",
                                 "Medusah_Black_-_07_-_Guilty_Pleasure.mp3",
                                 "Space_Heater_-_04_-_Mode_7.mp3"]
