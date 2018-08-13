068b7bc8b85dc831318a7648003a505ee8d78c40
zero <zero.griffin@gmail.com>
2018-08-13 10:24:15 +0200

    Update ssl player

--- Demo/Source/Constants.swift
+++ Demo/Source/Constants.swift
@@ -5,7 +5,7 @@ struct Constants {
     static let remote:[String] = ["http://tchakabam.com/test-media/m4a/shalafon.mp3",
                                 "http://tchakabam.com/test-media/m4a/Theo Katzman - Heartbreak Hits - 08 As the Romans Do.mp3",
                                 "http://tchakabam.com/test-media/m4a/guitars.m4a"]
-    static let remoteSsl:[String] = ["https://github.com/cdmx/MiniMancera/raw/master/miniMancera/Support/Assets/sounds/soundWhistlesVsZombies.caf"]
+    static let remoteSsl:[String] = ["https://github.com/mediaelement/mediaelement-files/raw/master/AirReview-Landmarks-02-ChasingCorporate.mp3"]
     static let localList:[String] = ["Black_Acid_Prophecy_-_14_-_Servant_of_the_Storm.mp3",
                                 "Medusah_Black_-_07_-_Guilty_Pleasure.mp3",
                                 "Space_Heater_-_04_-_Mode_7.mp3"]
